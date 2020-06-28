a3ff1349e6fecd058da9aa74db3f6282c0f1ca3f
Ridwan <ridwan@bmsit.ac.in>
2020-06-28 22:32:27 +0530

    fix toggle

--- assets/js/darkmode.js
+++ assets/js/darkmode.js
@@ -1,26 +1,32 @@
-let sun = '<img src="/assets/sun.png" width="40px"></img>';
-console.log(sun);
 let moon = '<img src="/assets/moon.png" width="40px"></img>';
+let sun = '<img src="/assets/sun.png" width="40px"></img>';
 
 let toggle = document.getElementById('theme-toggle');
+toggle.innerHTML=moon;
 const theme = localStorage.getItem('theme');
 
     if (theme === "dark") {
       document.body.setAttribute('data-theme', 'dark');
-    }
+      toggle.innerHTML=sun;
+      }
+
+
+
+
+    const userPrefers = getComputedStyle(document.documentElement).getPropertyValue('content');
 
     if (theme === "dark") {
-      document.getElementById("theme-toggle").innerHTML = moon;
+      toggle.innerHTML = sun;
     } else if (theme === "light") {
-      document.getElementById("theme-toggle").innerHTML = sun;
+      toggle.innerHTML = moon;
     } else if  (userPrefers === "dark") {
       document.body.setAttribute('data-theme', 'dark');
       window.localStorage.setItem('theme', 'dark');
-      document.getElementById("theme-toggle").innerHTML = moon;
+      toggle.innerHTML = moon;
     } else {
       document.body.setAttribute('data-theme', 'light');
       window.localStorage.setItem('theme', 'light');
-      document.getElementById("theme-toggle").innerHTML = sun;
+      toggle.innerHTML = sun;
     }
 
     function modeSwitcher() {
@@ -28,10 +34,10 @@ const theme = localStorage.getItem('theme');
       if (currentMode === "dark") {
         document.body.setAttribute('data-theme', 'light');
         window.localStorage.setItem('theme', 'light');
-        document.getElementById("theme-toggle").innerHTML = moon;
+        toggle.innerHTML = moon;
       } else {
         document.body.setAttribute('data-theme', 'dark');
         window.localStorage.setItem('theme', 'dark');
-        document.getElementById("theme-toggle").innerHTML = sun;
+        toggle.innerHTML = sun;
       }
     }
